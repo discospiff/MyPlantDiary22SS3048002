df69c42054292bca093bc83df70886dfcd2d89b3
Brandan <brandan.jones@uc.edu>
2022-03-03 08:34:33 -0500

    Create Spinner to show Specimen Objects.

--- app/src/main/java/app/plantdiary/myplantdiary22ss3048002/dto/Specimen.kt
+++ app/src/main/java/app/plantdiary/myplantdiary22ss3048002/dto/Specimen.kt
@@ -1,4 +1,7 @@
 package app.plantdiary.myplantdiary22ss3048002.dto
 
 data class Specimen(var plantName: String = "", var plantID: Int = 0, var specimenID : String = "", var location: String = "", var description : String = "", var datePlanted : String = "") {
+    override fun toString(): String {
+        return "$plantName"
+    }
 }
